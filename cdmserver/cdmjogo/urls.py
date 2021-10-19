fbc526e96fb80d69844e03566b74d24609ccccbc
Carlos Ribeiro (notebook CDM) <carribeiro@gmail.com>
2021-10-19 18:22:57 -0300

    Erro simples em views.py (metodo errado(

--- cdmserver/cdmjogo/urls.py
+++ cdmserver/cdmjogo/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('pulso_abrir_geladeira', views.pulso_abrir_geladeira, name='pulso_abrir_geladeira'), 
     path('pulso_abrir_gaveta_banheiro', views.pulso_abrir_gaveta_banheiro, name='pulso_abrir_gaveta_banheiro'), 
     path('pulso_abrir_gaveta_armario', views.pulso_abrir_gaveta_armario, name='pulso_abrir_gaveta_armario'), 
-    path('pulso_abrir_porta_armario', views.pulso_abrir_armario_armario, name='pulso_abrir_porta_armario'), 
+    path('pulso_abrir_porta_armario', views.pulso_abrir_porta_armario, name='pulso_abrir_porta_armario'), 
     path('pulso_abrir_gaveta_aparador', views.pulso_abrir_gaveta_aparador, name='pulso_abrir_gaveta_aparador'), 
     path('pulso_abrir_bau', views.pulso_abrir_bau, name='pulso_abrir_bau'), 
 ]
