48e433aca813c22b468f9337677c69636c4471e7
Carlos Ribeiro (notebook CDM) <carribeiro@gmail.com>
2021-10-20 20:21:11 -0300

    Loop de teste_diversos acelerado ao máximo; contador de pulsos para mesa de passar

--- cdmserver/cdmjogo/static/js/scriptdiversos.js
+++ cdmserver/cdmjogo/static/js/scriptdiversos.js
@@ -11,8 +11,10 @@ function updateStatus(resposta) {
     document.querySelector('#status-reed-bicicleta').className = ((resposta.bicicleta > 0) ? "dot dotVerde" : "dot dotCinza");
     document.querySelector('#count-reed-bicicleta').innerHTML = resposta.bicicleta;
     document.querySelector('#status-tomadas-armario').className = (resposta.tomadas_armario ? "dot dotVerde" : "dot dotCinza");
-    document.querySelector('#status-mesa-passar').className = (resposta.mesa_passar ? "dot dotVerde" : "dot dotCinza");
-    document.querySelector('#status-quiz-geladeira').className = (resposta.geladeira ? "dot dotVerde" : "dot dotCinza");
+    document.querySelector('#status-mesa-passar').className = ((resposta.mesa_passar > 0) ? "dot dotVerde" : "dot dotCinza");
+    document.querySelector('#count-mesa-passar').innerHTML = resposta.mesa_passar;
+    document.querySelector('#status-quiz-geladeira').className = ((resposta.geladeira > 0) ? "dot dotVerde" : "dot dotCinza");
+    document.querySelector('#count-quiz-geladeira').innerHTML = resposta.geladeira;
     document.querySelector('#status-botao-aparador').className = (resposta.aparador ? "dot dotVerde" : "dot dotCinza");
 
 }
--- cdmserver/cdmjogo/views.py
+++ cdmserver/cdmjogo/views.py
@@ -265,18 +265,28 @@ def read_status_tomadas_armario():
             return True
     """
 
+pulso_mesa_passar = 0
+
 def read_status_mesa_passar():
+    global pulso_mesa_passar
     mcp.confRegistradoresBanheiroAberto()
     gp_mesa_passar = 2 # GPB2 (MCP23017 0x22)
-    mcp.setup(gp_mesa_passar, mcp.GPB, mcp.IN, mcp.ADDRESS1)
-    return mcp.input(gp_mesa_passar, mcp.GPB, mcp.ADDRESS1) == 0
+    mcp.setup(gp_mesa_passar, mcp.GPB, mcp.IN, mcp.ADDRESS1) 
+    if (mcp.input(gp_mesa_passar, mcp.GPB, mcp.ADDRESS1) == 0):
+        pulso_mesa_passar += 1
+    return pulso_mesa_passar
+
+pulso_quiz_geladeira = 0
 
 def read_status_botao_geladeira():
+    global pulso_quiz_geladeira
     gpio_botao_geladeira = 32 # GPIO 32 (Raspberry Pi)
     GPIO.setmode(GPIO.BOARD) # Contagem de (0 a 40)
     GPIO.setwarnings(False) # Desativa avisos
-    GPIO.setup(gpio_botao_geladeira, GPIO.IN) # Pino como PULL-DOWN interno
-    return (GPIO.input(gpio_botao_geladeira) == 0)
+    GPIO.setup(gpio_botao_geladeira, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    if (GPIO.input(gpio_botao_geladeira) == 0):
+        pulso_quiz_geladeira += 1
+    return pulso_quiz_geladeira
 
 def read_status_botao_aparador():
     gp_botao_aparador = 5 # GPB5 (MCP23017 0x22)
